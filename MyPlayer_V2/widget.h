7dc6d55bf28e798124bcb5a7a74212eecaf80a54
ThreeDog <602985142@qq.com>
2017-06-02 17:12:27 +0800

    Complete basic controls shown
    Complete basic controls shown

--- MyPlayer_V2/widget.h
+++ MyPlayer_V2/widget.h
@@ -1,69 +1,54 @@
+/**************************************************************
+ * File Name   : widget.h
+ * Author      : ThreeDog
+ * Date        : Fri Jun 02 09:46:52 2017
+ * Description : 音乐播放器的主文件，主要的控件加载和业务逻辑的处理都在此类中进行。
+ *
+ **************************************************************/
+
 #ifndef WIDGET_H
 #define WIDGET_H
 #include "ThreeDog/tdwidget.h"
-#include "ThreeDog/tdslider.h"
-#include "ThreeDog/tdlistwidget.h"
 #include "ThreeDog/tdpushbutton.h"
-#include "ThreeDog/tdstackbutton.h"
 #include "ThreeDog/tdtoolbar.h"
-#include <QWidget>
-#include <QIcon>
-#include <QStandardPaths>
-#include <QMediaPlayer>
-#include <QMediaPlaylist>
-#include <QSystemTrayIcon>
-#include <QLabel>
-#include <QBitmap>
+#include "ThreeDog/tdstackbutton.h"
+#include "ThreeDog/tdslider.h"
+
 class Widget : public TDWidget
 {
+
     Q_OBJECT
 
 public:
-    Widget(QWidget *parent = 0);
-    //界面初始化函数
-    void initInterface();
-    //读取播放列表
-    void readMusicList(QStringList list);
-    //加载按钮
-    void loadButtons();
+    Widget(QWidget *parent = 0);         //构造函数
+    void InitInterface();                //初始化界面
+    void loadButtons();                  //加载按钮
     ~Widget();
 private :
-    //用户自定义播放列表的播放路径
-    QString m_sSavePath;
-    int m_iVolume;  //音量
-    QMediaPlayer * m_pPlayer;//播放器
-    QMediaPlaylist * m_pPlayList;//播放列表
-    QSystemTrayIcon *m_pTrayIcon;//系统托盘图标
-    QLabel *m_pMusicInfo;//音乐信息
-    QTimer *m_pTimer;//定时器
-    QLabel *m_pLeftMusicTime;//播放时间
-    QLabel *m_pRightMusicTime;//歌曲时间
-    QMenu *systray_menu;//托盘按钮
-    QAction *open_mainwindow;//打开主窗体动作
-    QAction *exit;//关闭动作
+    TDPushButton  *m_pPlayButton;        //播放按钮
+    TDPushButton  *m_pPauseButton;       //暂停按钮
+    TDPushButton  *m_pPreviousButton;    //上一曲按钮
+    TDPushButton  *m_pNextButton;        //下一曲按钮
+    TDPushButton  *m_pAddButton;         //添加按钮
+    TDPushButton  *m_pSettingButton;     //设置按钮
+    TDPushButton  *m_pSearchButton;      //查询按钮
+    TDPushButton  *m_pCloseButton;       //关闭按钮
+    TDPushButton  *m_pMinisizeButton;    //最小化按钮
+    TDToolbar     *m_pRemoteButton;      //遥控按钮
+    TDToolbar     *m_pLyricButton;       //歌词按钮
+    TDStackButton *m_pPlayModeButton;    //播放模式按钮
+    TDPushButton  *m_pModeButton1;       //顺序播放
+    TDPushButton  *m_pModeButton2;       //列表循环
+    TDPushButton  *m_pModeButton3;       //随机播放
+    TDPushButton  *m_pModeButton4;       //单曲循环
 
-    TDSlider * m_pMusicSlider;//歌曲播放进度条
-    TDSlider * m_pVolumeSlider;//音量槽
-    TDListWidget *m_pMusicList;//自定义音乐列表
-    //Cube *cube;
-    //MyLrc *lrc;
-    //QMap<qint64, QString> lrc_map;
-    //判断是否能够切换模式
-    //bool change_mode;
-    //bool network_start;
+    TDSlider      *m_pMusicSlider;       //音乐进度槽
+    TDSlider      *m_pVolumnSlider;      //音量槽
 
-    TDPushButton *m_pPlayButton;
-    TDPushButton *m_pNextButton;
-    TDPushButton *m_pPreviousButton;
-    TDPushButton *m_pAddButton;
-    TDPushButton *m_pCloseButton;
-    TDToolbar    *m_pLyricButton;
-    TDPushButton *m_pMiniButton;
-    TDPushButton *m_pPauseButton;
-    TDToolbar    *m_pRemoteControlButton;
-    TDPushButton *m_pSearchButton;
-    TDPushButton *m_pSettingButton;
-    TDStackButton * m_pPlayModeButton;
+    QLabel        *m_pMusicInfo;         //音乐名标签
+    QLabel        *m_pLeftMusicTime;     //左侧音乐时间
+    QLabel        *m_pRightMusicTime;    //右侧音乐时间
+
 };
 
 #endif // WIDGET_H
